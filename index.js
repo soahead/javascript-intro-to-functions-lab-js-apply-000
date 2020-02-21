7b936c4d5988fe9280b18f4b60a5f0a310d00944
Adrian Head <soahead@gmail.com>
2020-02-21 01:21:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string) {
+<<<<<<< HEAD
   console.log(string.toUpperCase());
 }
 function logWhisper(string) {
@@ -19,3 +20,21 @@ function sayHiToGrandma(string) {
   else if (string === "I love you, Grandma.")
   return "I love you, too.";
 }
+=======
+  console.log("HELLO");
+}
+function logWhisper(string) {
+  console.log("hello");
+}
+function sayHiToGrandma(string) {
+  var lowercase = 'hello'
+  lowercase.toLowerCase() === lowercase
+  string = "I can\'t hear you!"
+  return string 
+function sayHiToGrandma(string) {
+  var mixedCase = "I can\'t hear you!"
+  mixedCase.toUpperCase() === uppercase
+  string = "YES INDEED!"
+  return string
+}}
+>>>>>>> 3846c83c99fd55b306e4643d74ba37e17baf8e53
